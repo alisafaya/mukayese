5aad1e945ef569358ef5ad329952c36752921fed
Ali Safaya <alisafaya@gmail.com>
2023-05-01 16:01:02 +0300

    modify styles

--- app/src/screens/Tasks/Slides.js
+++ app/src/screens/Tasks/Slides.js
@@ -1,28 +1,34 @@
 import React from "react";
 
 import config from "../../config/index.json";
-import Divider from "./Divider";
 
 import { SlidesContainer, FlexWrapContainer, FlexWrapReverseContainer } from "./Tasks.styled"
 
 const Product = () => {
   const { product } = config;
   const [firstItem, secondItem] = product.items;
+  const bib = `@inproceedings{safaya-etal-2022-mukayese,
+    title = "Mukayese: {T}urkish {NLP} Strikes Back",
+    author = "Safaya, Ali  and
+      Kurtulu{\\c{s}}, Emirhan  and
+      Goktogan, Arda  and
+      Yuret, Deniz",
+    booktitle = "Findings of the Association for Computational Linguistics: ACL 2022",
+    month = may,
+    year = "2022",
+    address = "Dublin, Ireland",
+    publisher = "Association for Computational Linguistics",
+    url = "https://aclanthology.org/2022.findings-acl.69",
+    doi = "10.18653/v1/2022.findings-acl.69",
+    pages = "846--863",
+}`;
 
   return (
     <SlidesContainer id="product">
       <div className={"slides-main-div"}>
-        {/* <h1>
-          {product.title.split(' ').map((word, index) => (
-            <span
-              key={index}
-              className={index > 0 ? 'span2' : 'span1'}
-            >
-              {word}{' '}
-            </span>
-          ))}
-        </h1> */}
-        <Divider />
+        <h1 className={"slides-title"}>
+        📄 Publication
+        </h1>
         <FlexWrapContainer>
           <div className={"img-div"}>
             <a href={firstItem?.url}>
@@ -34,15 +40,17 @@ const Product = () => {
             </a>
           </div>
           <div className={"text-div"}>
-            <h3>
+            <pre>
+              {bib}
+            </pre>
+            {/* <h3>
               {firstItem?.title}
             </h3>
             <br />
-            {/* for each line in description print a paragraph */
               firstItem?.description.split('\n').map((line, index) => (
                 <p key={index}>{line}</p>
               )) 
-            }
+            */}
           </div>
         </FlexWrapContainer>
         <FlexWrapReverseContainer>
